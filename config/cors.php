6770b7ff82799211348c464f1ab59e67f99fae56
KimThanh1801 <hokimthanh1234@gmail.com>
2026-04-09 09:12:08 +0700

    fix axios baseURL for production

--- config/cors.php
+++ config/cors.php
@@ -10,6 +10,7 @@ return [
         'http://localhost:8000',
         'http://127.0.0.1:5173',
         'http://localhost:5173',
+        'https://projectciiclogroup-8.onrender.com',
     ],
 
     'allowed_headers' => ['*'],
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,27 +1,3 @@
-<template>
-  <div class="flex justify-center items-center min-h-screen bg-gray-100">
-    <div class="bg-white p-6 rounded-xl shadow w-100">
-
-      <h1 class="text-blue-500 text-2xl mb-4 text-center">
-        Register a new account
-      </h1>
-
-      <div class="flex flex-col gap-4 mt-2">
-        <input v-model="name" class="p-2 border rounded-md" placeholder="Enter name" />
-        <input v-model="email" class="p-2 border rounded-md" placeholder="Enter email" />
-        <input v-model="password" type="password" class="p-2 border rounded-md" placeholder="Enter password" />
-        <input v-model="confirmPassword" type="password" class="p-2 border rounded-md" placeholder="Confirm password" />
-      </div>
-
-      <div class="flex justify-center mt-4">
-        <button class="bg-blue-500 text-white p-2 rounded-md" @click="handleRegister">
-          Register
-        </button>
-      </div>
-
-    </div>
-  </div>
-</template>
 <script setup lang="ts">
 import { ref } from 'vue'
 import http from '@/services/http'
@@ -30,6 +6,7 @@ const name = ref('')
 const email = ref('')
 const password = ref('')
 const confirmPassword = ref('')
+const isButtonDisabled = ref(false);
 
 const handleRegister = async () => {
   try {
@@ -49,3 +26,30 @@ const handleRegister = async () => {
 }
 }
 </script>
+
+
+<template>
+  <div class="flex justify-center items-center min-h-screen bg-gray-100">
+    <div class="bg-white p-6 rounded-xl shadow w-100">
+
+      <h1 class="text-blue-500 text-2xl mb-4 text-center">
+        Register a new account
+      </h1>
+
+      <div class="flex flex-col gap-4 mt-2">
+        <input v-model="name" class="p-2 border rounded-md" placeholder="Enter name" />
+        <input v-model="email" class="p-2 border rounded-md" placeholder="Enter email" />
+        <input v-model="password" type="password" class="p-2 border rounded-md" placeholder="Enter password" />
+        <input v-model="confirmPassword" type="password" class="p-2 border rounded-md" placeholder="Confirm password" />
+      </div>
+
+      <div class="flex justify-center mt-4">
+        <button class="bg-blue-500 text-white p-2 rounded-md" @click="handleRegister"
+        :disabled="isButtonDisabled">
+          Register
+        </button>
+      </div>
+
+    </div>
+  </div>
+</template>
