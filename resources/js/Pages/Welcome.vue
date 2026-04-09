18336e536b6166862ae023eeb528093aa3958ba2
KimThanh1801 <hokimthanh1234@gmail.com>
2026-04-09 11:32:15 +0700

    feat(ui): implement homepage layout with header and hero section

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,53 @@
+<template>
+    <header class="border-b-2 border-gray-200">
+        <div class="flex justify-between items-center px-[180px] py-2">
+            <div class="p-4">
+                HomePage
+            </div>
+            <div class="flex items-center gap-10 p-4">
+                <a>About</a>
+                <a>Resume</a>
+                <a>Services</a>
+                <a>Portfolio</a>
+                <a>Dropdown</a>
+                <a>Contact</a>
+            </div>
+        </div>
+    </header>
+
+    <div class="px-[180px]">
+        <div class="flex items-center justify-between p-10">
+            <div class="w-1/2">
+                <p class="p-1 bg-gradient-to-r from-[#F15922] to-[#FFEFE9] rounded-2xl text-white text-center  mb-4">
+                    Hi, I'm Thanh
+                </p>
+
+                <h1 class="text-5xl font-bold">
+                    I build modern and user-friendly web applications
+                </h1>
+
+                <ul class="mt-4 text-gray-600">
+                    <li class="mb-4">
+                        I am an IT student passionate about building beautiful and user-friendly websites.
+                    </li>
+                    <li class="mb-4">
+                        I have experience with HTML, CSS, JavaScript, and Laravel.
+                    </li>
+                    <li class="mb-4">
+                        I enjoy creating responsive and intuitive UI/UX.
+                    </li>
+                </ul>
+
+                <button class="mt-4 bg-[#F15922] text-white rounded-sm p-2">
+                    Explore >
+                </button>
+            </div>
+
+            <div class="w-1/2 flex justify-center ">
+                <img src="../../../public/images/image-personal.png" alt="" class="w-[1500px]">
+            </div>
+        </div>
+    </div>
+
+    <footer></footer>
+</template>
